e024a384e255fdc2e6a7fdf9cf975af9f3cd12ca
rafael osawa <osawa.rafael@gmail.com>
2021-04-14 16:54:47 +0300

    removed /test folder

--- spec/models/person_spec.rb
+++ spec/models/person_spec.rb
@@ -2,11 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Person, type: :model do
   it "is valid with valid attributes" do
-    expect(Auction.new).to be_valid
+    expect(Person.new).to be_valid
   end
 
-  it "is not valid without a name" do
-    person = Person.new(title: nil)
-    expect(person).to_not be_valid
-  end
 end
